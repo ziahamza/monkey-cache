6de9b370382f6e5d401dba46220ff5ac06ff9749
hamza zia <ziahamza2007@gmail.com>
2013-07-20 17:53:04 +0200

    added mmap support, serving files from there

--- cache.c
+++ cache.c
@@ -10,6 +10,11 @@
 #include <netdb.h>
 #include <fcntl.h>
 
+#include <sys/mman.h>
+#define MMAP_SIZE sysconf(_SC_PAGE_SIZE)
+
+#include <math.h>
+
 #include "MKPlugin.h"
 
 MONKEY_PLUGIN("cache",         /* shortname */
@@ -24,6 +29,8 @@ struct cache_req_t {
     struct session_request *sr;
 
     int fd_file;
+    void * mmap;
+    long mmap_len;
     long bytes_offset, bytes_to_send;
 
     struct mk_list _head;
@@ -45,13 +52,13 @@ struct cache_req_t * cache_reqs_new() {
     return req;
 }
 
-struct cache_req_t * cache_reqs_get(int socket, struct session_request *sr) {
+struct cache_req_t * cache_reqs_get(int socket) {
   struct mk_list *reqs = pthread_getspecific(cache_reqs);
   struct mk_list *curr;
 
   mk_list_foreach(curr, reqs) {
     struct cache_req_t *req = mk_list_entry(curr, struct cache_req_t, _head);
-    if (req->socket == socket && req->sr == sr) {
+    if (req->socket == socket) {
       return req;
     }
   }
@@ -69,8 +76,9 @@ void cache_reqs_del(int socket) {
       mk_info("removing a requet!");
       mk_list_del(&req->_head);
       free(req);
-    }
+    };
   }
+
 }
 
 
@@ -103,6 +111,10 @@ void _mkp_core_thctx() {
 
 int http_send_file(struct cache_req_t *req)
 {
+    if (req->bytes_to_send <= 0) {
+        mk_info("no data to send, returning from send file!");
+        return 0;
+    }
 
     long nbytes = mk_api->socket_send_file(req->socket, req->fd_file,
                                  &req->bytes_offset, req->bytes_to_send);
@@ -120,10 +132,68 @@ int http_send_file(struct cache_req_t *req)
     return req->bytes_to_send;
 }
 
+int http_send_mmap(struct cache_req_t *req) {
+
+    if (req->bytes_to_send <= 0) {
+        mk_info("no data to send, returning from send file!");
+        return 0;
+    }
+
+    long nbytes = mk_api->socket_send(req->socket, req->mmap + req->bytes_offset, req->bytes_to_send);
+
+    if (nbytes < 0) {
+        perror("cannot write to socket!");
+        return -1;
+    }
+
+    printf("sending %ld bytes!\n", nbytes);
+
+    req->bytes_to_send -= nbytes;
+    req->bytes_offset += nbytes;
+    return req->bytes_to_send;
+}
+
+int _mkp_event_write(int fd) {
+    struct cache_req_t *req = cache_reqs_get(fd);
+    if (!req) {
+        mk_info("write event, but not of our request");
+        return MK_PLUGIN_RET_EVENT_NEXT;
+    }
+
+    mk_info("can write to our  request!");
+    int ret = 0;
+    if (req->mmap != NULL) {
+        mk_info("sending using mmap :)");
+        ret = http_send_mmap(req);
+    }
+    else {
+        mk_info("mmap invalid, sending standard file");
+        ret = http_send_file(req);
+    }
+
+    if (ret <= 0) {
+        mk_info("closing file fd and ending event write");
+        if (req->fd_file >= 0)
+            close(req->fd_file);
+
+        if (req->mmap != NULL)
+            munmap(req->mmap, req->mmap_len);
+
+        return MK_PLUGIN_RET_EVENT_CLOSE;
+    }
+    else {
+        mk_info("send some data, iterating event write again!");
+        mk_api->http_request_end(req->socket);
+        return MK_PLUGIN_RET_EVENT_OWNED;
+    }
+}
+
 int _mkp_stage_30(struct plugin *plugin, struct client_session *cs,
                   struct session_request *sr)
 {
     (void) plugin;
+
+    mk_info("running stage 30");
 
     if (sr->file_info.size < 0 || sr->file_info.is_file == MK_FALSE || sr->file_info.read_access == MK_FALSE || sr->method != HTTP_METHOD_GET) {
         mk_info("not a file, passing on the file :)");
@@ -146,13 +216,17 @@ int _mkp_stage_30(struct plugin *plugin, struct client_session *cs,
 
     // sr->fd_file = open(sr->real_path.data, sr->file_info.flags_read_only);
     req->fd_file = open(sr->real_path.data, sr->file_info.flags_read_only);
+    int size = ceil((double)sr->file_info.size / MMAP_SIZE) * MMAP_SIZE;
+    req->mmap = mmap(NULL, size, PROT_READ, MAP_PRIVATE, req->fd_file, 0);
+    req->mmap_len = size;
 
     if (req->fd_file == -1) {
         return MK_PLUGIN_RET_NOT_ME;
     }
 
     // sr->bytes_to_send = sr->file_info.size;
-    sr->bytes_to_send = req->bytes_offset = 0;
+    //sr->bytes_to_send = req->bytes_offset = 0;
+    req->bytes_offset = 0;
     req->bytes_to_send = sr->file_info.size;
 
     sr->headers.content_type = mk_default_mime;
@@ -165,15 +239,34 @@ int _mkp_stage_30(struct plugin *plugin, struct client_session *cs,
 
 int _mkp_stage_40(struct client_session *cs, struct session_request *sr) {
 
-  struct cache_req_t *req = cache_reqs_get(cs->socket, sr);
+  struct cache_req_t *req = cache_reqs_get(cs->socket);
   if (req == NULL) {
     mk_info("not our request from stage 40!");
     return MK_PLUGIN_RET_NOT_ME;
   }
 
-  if (http_send_file(req) <= 0) {
+  mk_info("our request, but we are still passing on from STAGE 40!");
+
+  return MK_PLUGIN_RET_NOT_ME;
+
+  int ret = 0;
+  if (req->mmap != 0) {
+    mk_info("sending using mmap :)");
+    ret = http_send_mmap(req);
+  }
+  else {
+    mk_info("mmap invalid, sending standard file");
+    ret = http_send_file(req);
+  }
+
+  if (ret <= 0) {
       mk_info("closing file fd and ending stage 40");
-      close(req->fd_file);
+      if (req->fd_file >= 0)
+        close(req->fd_file);
+      if (req->mmap != 0)
+        munmap(req->mmap, req->mmap_len);
+
+      sr->close_now = MK_TRUE;
       return MK_PLUGIN_RET_END;
   }
   else {
@@ -183,6 +276,7 @@ int _mkp_stage_40(struct client_session *cs, struct session_request *sr) {
 }
 
 int _mkp_stage_50(int socket) {
+    mk_info("trying to remove a socket, may not be ours");
     cache_reqs_del(socket);
 
     return 0;
